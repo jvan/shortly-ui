ab22631a52adf96acaf6ca6cbf48b66accc273cd
jvan <jordan.vanaalsburg@gmail.com>
2015-11-01 12:31:21 -0800

    Add linkCount computed property.

--- app/models/user.js
+++ app/models/user.js
@@ -2,5 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name:  DS.attr('string'),
-	links: DS.hasMany('link', {async: true})
+	links: DS.hasMany('link', {async: true}),
+
+	linkCount: function() {
+		return this.get('links.length');
+	}.property('links.[]')
 });
