0b0fd78ca453741e634f3e59f34195fc1a38422d
jvan <jordan.vanaalsburg@gmail.com>
2015-11-01 12:29:24 -0800

    Add shortUrl/Href computed properties.

--- app/models/link.js
+++ app/models/link.js
@@ -2,5 +2,14 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   url: DS.attr('string'),
-  key: DS.attr('string') 
+  key: DS.attr('string'),
+
+  shortUrl: function() {
+    return `http://short.ly/go/${this.get('key')}`;
+  }.property('key'),
+
+  shortHref: function() {
+    return `http://localhost:1323/go/${this.get('key')}`;
+  }.property('key')
+
 });
