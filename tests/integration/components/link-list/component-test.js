23625648eb1bb9c9d9e5f7f76ba6827b7e1f301c
jvan <jordan.vanaalsburg@gmail.com>
2015-11-01 14:29:23 -0800

    Add link-list component for display user links.

---
+++ tests/integration/components/link-list/component-test.js
@@ -0,0 +1,27 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+
+moduleForComponent('link-list', 'Integration | Component | link list', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{link-list}}`);
+
+  assert.equal(this.$().text(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#link-list}}
+      template block text
+    {{/link-list}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
