df012ab00bdc10d8cf818ae37b7efbe61c5fe6bd
jvan <jordan.vanaalsburg@gmail.com>
2015-11-01 15:12:23 -0800

    Display last click time for links.

--- app/models/link.js
+++ app/models/link.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   url: DS.attr('string'),
   key: DS.attr('string'),
+  last_click: DS.attr('date'),
 
   shortUrl: function() {
     return `http://short.ly/go/${this.get('key')}`;
