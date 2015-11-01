50ca6e796a697dcc261e79f77815b56c0a7ea1dc
jvan <jordan.vanaalsburg@gmail.com>
2015-11-01 15:46:14 -0800

    Add work around for non-clicked links.

--- app/helpers/elapsed-time.js
+++ app/helpers/elapsed-time.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export function elapsedTime(params/*, hash*/) {
   var time = params[0];
-  return moment().to(time);
+  if (moment(time).year() < 2015) {
+    return 'never clicked';
+  }
+  return `last clicked ${moment().to(time)}`;
 }
 
 export default Ember.Helper.helper(elapsedTime);
